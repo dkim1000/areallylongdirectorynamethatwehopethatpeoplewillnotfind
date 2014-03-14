96f9a1da272673eb1c097a36ac89d67df1ff7331
diablojohn <diablojohn@gmail.com>
2014-03-14 16:04:32 -0700

    added filedescriptor.c and filedescriptor.h
    wont crash since we assume we havent used yet (bmake wont realize it)

---
+++ cs350-os161/os161-1.99/kern/include/filedescriptor.h
@@ -0,0 +1,20 @@
+#ifndef FILEDESCRIPTOR_H_
+#define FILEDESCRIPTOR_H_
+
+struct filedescriptor{
+        struct lock *fdlock;
+        int fdmode;
+        struct vnode *fdvnode;
+        int addr;
+        int fdoff;
+        int fdopen;
+
+
+
+};
+
+struct filedescriptor *fdcreate(int off, unsigned int mode, char *nameOfLock, struct vnode *curvnode);
+
+void fddestroy(struct filedescriptor *file);
+#endif
+
---
+++ cs350-os161/os161-1.99/kern/vfs/filedescriptor.c
@@ -0,0 +1,40 @@
+#include <filedescriptor.h>
+#include <types.h>
+#include <synch.h>
+#include <lib.h>
+#include <vfs.h>
+#include <kern/unistd.h>
+
+struct filedescriptor *fdcreate(int off, unsigned int mode, char *nameOfLock, struct vnode *curvnode){
+	struct filedescriptor *file;
+	file = kmalloc(sizeof(struct filedescriptor));
+
+	file->addr = 1;
+	file->fdmode = mode;
+	file->fdlock = lock_create(nameOfLock);
+	file->fdoff = off;
+	file->fdvnode = curvnode;
+	file->fdopen = 0;
+	
+	return file;
+	
+
+
+}
+
+void fddestroy(struct filedescriptor *file){
+
+
+	if(file->fdopen > 1){
+	file->fdopen = 0;
+	file->addr = 1;
+	
+	}else if (file->fdopen <= 1){
+	file->fdopen == 0; //closed
+	vfs_close(file->fdvnode);
+	kfree(file)
+	file->addr = 0;
+	}
+
+
+}
