bbecb4c877a275857ddb736ac3060b5b4cc1f515
David Kim <dkim1000@msn.com>
2014-03-15 03:28:41 -0400

    Added fdManager.h

---
+++ cs350-os161/os161-1.99/kern/include/fdManager.h
@@ -0,0 +1,20 @@
+#ifndef FILEMANAGER_H_
+#define FILEMANAGER_H_
+
+struct fdManager
+{
+	struct array *fdm_data;		// the handles to the file descriptors
+	int fdm_initialized;		// indicates if the file manager has been initialized
+	int fdm_next;
+};
+
+struct fdManager * make_fdManager();
+
+int initialize_fdManager(struct fdManager *manager);
+struct filedescriptor * getFileDescriptor(fdManager *manager, int index);
+int addDescriptor(struct fdManager *manager, struct filedescriptor *descriptor);
+int removeDescriptor(struct fdManager * manager, int index);
+struct fdManager * clone(struct fdManager *manager);
+int getSize(struct fdManager * manager);
+void destroy(struct fdManager * manager);
+void print(struct fdManager * manager);
